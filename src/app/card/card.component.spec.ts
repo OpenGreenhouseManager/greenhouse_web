1f5830811f04f2c8756040b186585b32ce14fce2
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-08-16 15:39:25 +0200

    fix tests

--- src/app/card/card.component.spec.ts
+++ src/app/card/card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -9,10 +10,12 @@ describe('CardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CardComponent],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('title', 'test');
     fixture.detectChanges();
   });
 
