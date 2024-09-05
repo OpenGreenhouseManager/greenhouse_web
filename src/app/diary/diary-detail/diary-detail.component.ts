7a7f764f7d13a6ba385001c52040b4e0e65a0f8c
Daniel Mauderer <daniel.mauderer@outlook.com>
2024-09-05 13:15:12 +0200

    diary detail

---
+++ src/app/diary/diary-detail/diary-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, computed } from '@angular/core';
+import { NavBarComponent } from '../../nav_bar/nav_bar.component';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../../card/card.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DiaryService } from '../services/diary-service';
+
+@Component({
+  selector: 'grn-diary-detail',
+  standalone: true,
+  imports: [
+    NavBarComponent,
+    CommonModule,
+    CardComponent,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+  ],
+  templateUrl: './diary-detail.component.html',
+  styleUrl: './diary-detail.component.scss',
+})
+export class DiaryDetailComponent {
+  private id = this.route.snapshot.paramMap.get('id');
+  diary = computed(() => {
+    return this.diaryService.getDiary(this.id?.toString() || '');
+  });
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private diaryService: DiaryService
+  ) {}
+
+  editEntry() {
+    this.router.navigate(['diary', this.id, 'edit']);
+  }
+}
