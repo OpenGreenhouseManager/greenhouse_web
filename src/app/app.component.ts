e639986437efbda84934f54be62ad40c776ed4f3
Daniel Mauderer <daniel.mauderer@outlook.com>
2025-09-13 00:44:24 +0200

    add envs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'grn-root',
@@ -13,5 +14,11 @@ import { ButtonModule } from 'primeng/button';
 export class AppComponent {
   public str: string = $localize`:@@HOME-TEXT1:Add some content to get startedd!`;
   public str2: string = $localize`:@@HOME-TEXT2:this is some more localization!`;
+
+  constructor() {
+    console.log(environment.production); // Logs false for development environment
+    console.log('using api: ' + environment.baseUrl);
+  }
+
   title = 'greenhouse_web';
 }
